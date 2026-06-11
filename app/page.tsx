97844aee95949182fc31a32128537ca1ccd93564
Jan Del Olmo <jandelolmo@gmail.com>
2026-06-11 14:13:10 +0200

    añadido selector de archivo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+
   return (
-    <div>
-      <h1>Pokemon Randomizer</h1>
-      <p>Mi primera web funciona.</p>
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">
+        Pokemon Randomizer
+      </h1>
+
+      <input
+        type="file"
+        onChange={(e) => {
+          const selectedFile = e.target.files?.[0];
+
+          if (selectedFile) {
+            setFile(selectedFile);
+          }
+        }}
+      />
+
+      {file && (
+        <p>
+          Archivo seleccionado: {file.name}
+        </p>
+      )}
+    </main>
   );
 }
